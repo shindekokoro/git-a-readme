49e0e4730b5f392cb0b930f1a0cc26cc71d84a7f
Brian Whisler <brian.whisler@gmail.com>
2023-08-12 20:19:09 -0600

    Add questions, and start writting functions

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./modules/generateMarkdown');
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    // Get list of licenses 
+    const licenses = await generateMarkdown.getLicenseList();
+
+    // Questions for user input
+    // Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+    const questions = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the Project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: (question) => `Description of '${question.title}'?`
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How do you install your project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Does your project have any special usage information?'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: (question) => `What are '${question.title}'s contribution guidelines?`
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Are there any specific test instructions?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Pick a LICENSE for your project.',
+            choices: licenses.map(license => license.name)
+        }
+    ];
+
+
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.error('Prompt cannot be rendered in the current environment.');
+            } else {
+                console.error('Something went wrong %s', error);
+            }
+        });
+}
 
 // Function call to initialize app
 init();
--- modules/generateMarkdown.js
+++ modules/generateMarkdown.js
@@ -1,14 +1,53 @@
+const axios = require('axios');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license, username, repository) {
+  if (!license || !username || !repository) {
+    return '';
+  } else {
+    return `https://img.shields.io/github/license/${username}/${repository}`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  } else {
+    // Hard Coded for testing, get link from github api
+    return 'https://api.github.com/licenses/mpl-2.0';
+  }
+}
+
+
+// Return List of most commonly used licenses from github api
+async function getLicenseList() {
+  let license = await axios.get('https://api.github.com/licenses');
+  return license.data;
+}
+
+// Is there already a license file?
+function getCurrentLicense() {
+  let license = fs.readFile('LICENSE.md', 'utf8', (error, data) => {
+    switch (error.code) {
+      case 'ENOENT':
+        console.error('File doesn\'t exist');
+        break;
+      case undefined:
+        console.log(undefined);
+      default:
+        console.log(error);
+        break;
+    }
+  });
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -17,4 +56,7 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown,
+  getLicenseList
+}
